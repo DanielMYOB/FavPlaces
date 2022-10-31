19dead0739b6e5413edd7d5d3df0466eeb642d74
DanielMYOB <daniel.languiller@myob.com>
2022-10-31 15:53:28 +1100

    end section 4

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["eslint:recommended", "plugin:react/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react"],
+  rules: {
+    "react/react-in-jsx-scope": 0,
+    "react/jsx-uses-react": 0,
+    "react/prop-types": {
+      enabled: 0,
+    },
+  },
+};
---
+++ README.md
@@ -0,0 +1,1 @@
+#### Source Code: https://github.com/academind/react-native-practical-guide-code/commit/2eba797e43a551549adaf587225d2bf34c54f21c
